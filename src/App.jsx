17d9b025db7a7f3f9023479aaab9c9215aab9b33
devdhacoder <adigunolamide200@gmail.com>
2021-12-01 14:40:39 +0100

    Feat: initial commit message for the website

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './app.scss';
+
+// imported components
+import Header from './components/Header/Header';
+
+function App() {
+
+  return (
+
+    <div className="root--app">
+
+      <Header />
+
+    </div>
+
+  )
+
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./header.css";
+
+const header = () => {
+
+    return (
+
+        <header className="main--app--header">
+            
+        </header>
+
+    )
+
+}
+
+export default header
